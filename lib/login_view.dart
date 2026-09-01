0961bec548f8a661ca6fc604e173c6e228e70a18
Julio <julincapixaba@gmail.com>
2026-08-31 21:33:14 -0300

    feat: rascunho tela de login

---
+++ lib/login_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWinget{
+  const LoginView({super.key});
+}
+
+@override
+Winget build(BuildContext context) {
+  return const Scaffold(
+    
+  )
+}
