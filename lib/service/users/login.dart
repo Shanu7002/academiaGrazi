f4facafc64f80866406d02b3f20b22674904fb17
Edu Muller <eduardoaum2007@gmail.com>
2026-09-06 15:36:31 -0300

    fix: remove try catch from service so controller can handle erros properly

--- lib/service/users/login.dart
+++ lib/service/users/login.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'package:academiagrazi/models/users/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -9,21 +8,16 @@ class LoginService {
     : _db = db ?? FirebaseFirestore.instance;
 
   Future<UserModel?> getUserProfile(String uid) async {
-    try {
-      final DocumentSnapshot snapshot =
-          await _db.collection("users").doc(uid).get();
+    final DocumentSnapshot snapshot =
+        await _db.collection("users").doc(uid).get();
 
-      if (snapshot.exists && snapshot.data() != null) {
-        return UserModel.fromJson(
-          snapshot.data() as Map<String, dynamic>,
-          snapshot.id,
-        );
-      }
-
-      return null;
-    } catch (e) {
-      log("Database login failed", error: e);
-      return null;
+    if (snapshot.exists && snapshot.data() != null) {
+      return UserModel.fromJson(
+        snapshot.data() as Map<String, dynamic>,
+        snapshot.id,
+      );
     }
+
+    return null;
   }
 }
