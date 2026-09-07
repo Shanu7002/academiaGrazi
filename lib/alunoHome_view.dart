a4429c1a02a3901f86a043a3610585bcac97b8fc
Julio <julincapixaba@gmail.com>
2026-09-07 14:57:12 -0300

    feat: header do painel

--- lib/alunoHome_view.dart
+++ lib/alunoHome_view.dart
@@ -5,6 +5,9 @@ class AlunoHomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: const Color(0xFFD8D6D6),
       appBar: AppBar(
@@ -18,7 +21,10 @@ class AlunoHomeView extends StatelessWidget {
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+          padding: EdgeInsets.symmetric(
+            horizontal: screenWidth * 0.05,
+            vertical: 20.0,
+          ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -28,17 +34,126 @@ class AlunoHomeView extends StatelessWidget {
                   ClipRRect(
                     borderRadius: BorderRadius.circular(12),
                     child: Image.asset(
-                      'assets/retanguloVerde.png', // Substitua pela sua imagem
-                      height: 200,
+                      'assets/retanguloVerde.png',
+                      height: screenHeight * 0.50,
                       width: double.infinity,
                       fit: BoxFit.cover,
                     ),
                   ),
 
-                  // 2. Elementos por cima da imagem
                   Positioned(
-                    top: 10,
-                    child: Image.asset('assets/logoGraziBranca.png'),
+                    top: screenHeight * 0.02,
+                    child: SizedBox(
+                      height: screenHeight * 0.20,
+                      child: Image.asset('assets/logoGraziBranca.png'),
+                    ),
+                  ),
+
+                  SizedBox(height: screenHeight * 0.40),
+
+                  Positioned(
+                    bottom: screenHeight * 0.10,
+                    // IMPORTANTE: Adicionado width para o container não encolher no Positioned
+                    width: screenWidth * 0.60,
+                    child: Container(
+                      height: screenHeight * 0.15,
+                      // Removi o padding interno para as linhas divisórias encostarem perfeitamente na borda
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color.fromARGB(
+                              255,
+                              2,
+                              89,
+                              79,
+                            ).withOpacity(
+                              0.3,
+                            ), // Opacidade opcional para suavizar a sombra
+                            blurRadius: 6,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    child: const Text(
+                                      'Sequência 🔥',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    child: const Text(
+                                      'Progressão 📈',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+
+                          Expanded(
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    child: const Text(
+                                      'Dias',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    child: const Text(
+                                      'KG',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: screenHeight * 0.03,
+                    left: screenWidth * 0.05,
+                    child: const Text(
+                      'Bem vindo aluno!',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
                   ),
                 ],
               ),
