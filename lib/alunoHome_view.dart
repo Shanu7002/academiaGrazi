bd2fbbd17df4e5c0d28564a04320c7df1627076c
Julio <julincapixaba@gmail.com>
2026-09-08 16:59:30 -0300

    feat: widget treinos

--- lib/alunoHome_view.dart
+++ lib/alunoHome_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AlunoHomeView extends StatelessWidget {
   const AlunoHomeView({super.key});
@@ -19,16 +20,18 @@ class AlunoHomeView extends StatelessWidget {
         centerTitle: true,
         iconTheme: const IconThemeData(color: Colors.white),
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(
-            horizontal: screenWidth * 0.05,
-            vertical: 20.0,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Stack(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(
+          horizontal: screenWidth * 0.05,
+          vertical: 20.0,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: screenHeight * 0.50,
+              width: double.infinity,
+              child: Stack(
                 alignment: Alignment.center,
                 children: [
                   ClipRRect(
@@ -40,24 +43,18 @@ class AlunoHomeView extends StatelessWidget {
                       fit: BoxFit.cover,
                     ),
                   ),
-
                   Positioned(
                     top: screenHeight * 0.02,
                     child: SizedBox(
-                      height: screenHeight * 0.20,
+                      height: screenHeight * 0.18,
                       child: Image.asset('assets/logoGraziBranca.png'),
                     ),
                   ),
-
-                  SizedBox(height: screenHeight * 0.40),
-
                   Positioned(
-                    bottom: screenHeight * 0.10,
-                    // IMPORTANTE: Adicionado width para o container não encolher no Positioned
+                    bottom: screenHeight * 0.08,
                     width: screenWidth * 0.60,
                     child: Container(
-                      height: screenHeight * 0.15,
-                      // Removi o padding interno para as linhas divisórias encostarem perfeitamente na borda
+                      height: screenHeight * 0.14,
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(12.0),
@@ -68,9 +65,7 @@ class AlunoHomeView extends StatelessWidget {
                               2,
                               89,
                               79,
-                            ).withOpacity(
-                              0.3,
-                            ), // Opacidade opcional para suavizar a sombra
+                            ).withOpacity(0.3),
                             blurRadius: 6,
                             offset: const Offset(0, 3),
                           ),
@@ -88,7 +83,7 @@ class AlunoHomeView extends StatelessWidget {
                                       'Sequência 🔥',
                                       style: TextStyle(
                                         fontWeight: FontWeight.bold,
-                                        fontSize: 16,
+                                        fontSize: 14,
                                       ),
                                     ),
                                   ),
@@ -100,7 +95,7 @@ class AlunoHomeView extends StatelessWidget {
                                       'Progressão 📈',
                                       style: TextStyle(
                                         fontWeight: FontWeight.bold,
-                                        fontSize: 16,
+                                        fontSize: 14,
                                       ),
                                     ),
                                   ),
@@ -108,7 +103,6 @@ class AlunoHomeView extends StatelessWidget {
                               ],
                             ),
                           ),
-
                           Expanded(
                             child: Row(
                               children: [
@@ -119,7 +113,7 @@ class AlunoHomeView extends StatelessWidget {
                                       'Dias',
                                       style: TextStyle(
                                         fontWeight: FontWeight.bold,
-                                        fontSize: 16,
+                                        fontSize: 14,
                                       ),
                                     ),
                                   ),
@@ -131,7 +125,7 @@ class AlunoHomeView extends StatelessWidget {
                                       'KG',
                                       style: TextStyle(
                                         fontWeight: FontWeight.bold,
-                                        fontSize: 16,
+                                        fontSize: 14,
                                       ),
                                     ),
                                   ),
@@ -144,7 +138,7 @@ class AlunoHomeView extends StatelessWidget {
                     ),
                   ),
                   Positioned(
-                    bottom: screenHeight * 0.03,
+                    bottom: screenHeight * 0.02,
                     left: screenWidth * 0.05,
                     child: const Text(
                       'Bem vindo aluno!',
@@ -157,8 +151,118 @@ class AlunoHomeView extends StatelessWidget {
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+
+            const SizedBox(height: 30),
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Treino de hoje',
+                  style: GoogleFonts.antonio(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(width: 15),
+                Expanded(
+                  child: Image.asset(
+                    'assets/linhaGradiente.png',
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 20),
+
+            SizedBox(
+              height: screenHeight * 0.25,
+              width: double.infinity,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Positioned(
+                    bottom: 10,
+                    width: screenWidth * 0.85,
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color.fromARGB(
+                              255,
+                              2,
+                              89,
+                              79,
+                            ).withOpacity(0.3),
+                            blurRadius: 6,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              print("Botão laranja clicado!");
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color.fromARGB(
+                                255,
+                                244,
+                                121,
+                                67,
+                              ),
+                              foregroundColor: Colors.white,
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 16,
+                                vertical: 12,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                            ),
+
+                            child: Text(
+                              '4 exercícios',
+                              style: TextStyle(
+                                fontFamily: GoogleFonts.inter().fontFamily,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'TREINO B - INFERIORES',
+                            style: GoogleFonts.antonio(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: const Color.fromARGB(255, 2, 89, 79),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          const Text(
+                            'Quadríceps, posterior e panturrilha',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
