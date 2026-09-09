27037b8a1aafcc02a534ee20996f32f04eaf88bd
Bayer-07 <gabrielbayer2603@gmail.com>
2026-09-09 19:32:49 -0300

    feat: Implementei o menu para navegar entre as páginas que já existem 'tabRouter.dart'

---
+++ lib/view/user/alunoProfile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AlunoProfileView extends StatelessWidget {
+  const AlunoProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text("Olá Mundo"));
+  }
+}
